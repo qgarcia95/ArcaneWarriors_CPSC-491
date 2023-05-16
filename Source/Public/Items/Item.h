0af29b5a6cbc595e69261fea7ad7681e339ffadf
Quauhtli Garcia-Brindis <79949335+qgarcia95@users.noreply.github.com>
2023-05-15 23:56:24 -0700

    Item.h

---
+++ Source/Public/Items/Item.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+class USphereComponent; 
+class UNiagaraSystem;
+class UNiagaraComponent;
+
+enum class EItemState : uint8
+{
+	EIS_Hovering,
+	EIS_Equipped
+};
+
+UCLASS()
+class ARCANEWARRIORS_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AItem();
+	virtual void Tick(float DeltaTime) override;
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere , BluePrintReadWrite, Category = "Sine Parameters")
+	float Amplitude = 0.25f;
+
+	UPROPERTY(EditAnywhere, BluePrintReadWrite, Category = "Sine Parameters")
+	float TimeConstant = 5.f;
+
+	UFUNCTION(BlueprintPure)
+	float TransformedSin();
+
+	UFUNCTION(BlueprintPure)
+	float TransformedCos();
+
+	template<typename T>
+	T Avg(T First, T Second);
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	virtual void SpawnPickupSystem();
+	virtual void SpawnPickupSound();
+
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly)
+	UStaticMeshComponent* ItemMesh;
+
+	EItemState ItemState = EItemState::EIS_Hovering;
+
+	UPROPERTY(VisibleAnywhere)
+	USphereComponent* Sphere; 
+
+	UPROPERTY(EditAnywhere)
+	UNiagaraComponent* ItemEffect;
+
+	UPROPERTY(EditAnywhere)
+	USoundBase* PickupSound;
+
+private:
+	UPROPERTY(VisibleAnywhere, BluePrintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float RunningTime;
+
+	UPROPERTY(EditAnywhere)
+	UNiagaraSystem* PickupEffect;
+};
+
+template<typename T>
+inline T AItem::Avg(T First, T Second)
+{
+	return (First + Second)/2;}
