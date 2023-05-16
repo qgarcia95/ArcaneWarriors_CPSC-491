333b9bcc9f522f58f181e09c4197a7cb0705ccf5
Quauhtli Garcia-Brindis <79949335+qgarcia95@users.noreply.github.com>
2023-05-16 00:01:48 -0700

    HealthBar.h

---
+++ Source/Public/HUD/HealthBar.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HealthBar.generated.h"
+
+UCLASS()
+class ARCANEWARRIORS_API UHealthBar : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* HealthBar;	
+};
