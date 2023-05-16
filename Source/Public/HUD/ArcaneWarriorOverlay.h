6f1312d41de2c6484e3b0a13aa45619e7ccb8ff3
Quauhtli Garcia-Brindis <79949335+qgarcia95@users.noreply.github.com>
2023-05-16 00:01:11 -0700

    ArcaneWarriorOverlay.h

---
+++ Source/Public/HUD/ArcaneWarriorOverlay.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ArcaneWarriorOverlay.generated.h"
+
+UCLASS()
+class ARCANEWARRIORS_API UArcaneWarriorOverlay : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	void SetHealthBarPercent(float Percent);
+	void SetStaminaBarPercent(float Percent);
+	void SetGold(int32 Gold);
+	void SetSouls(int32 Souls);
+private:
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* HealthProgressBar;
+
+	UPROPERTY(meta = (BindWidget))
+	class UProgressBar* StaminaProgressBar;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* GoldText;
+
+	UPROPERTY(meta = (BindWidget))
+	class UTextBlock* SoulsText;	
+};
