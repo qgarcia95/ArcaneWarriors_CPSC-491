0894234ec5724de5a033d802b4dcbe5e9bef7af8
Quauhtli Garcia-Brindis <79949335+qgarcia95@users.noreply.github.com>
2023-05-16 00:22:46 -0700

    PickupInterface.cpp

---
+++ Source/Private/Interfaces/PickupInterface.cpp
@@ -0,0 +1,14 @@
+#include "Interfaces/PickupInterface.h"
+// Add default functionality here for any IPickupInterface functions that are not pure virtual.
+
+void IPickupInterface::SetOverlappingItem(AItem* Item)
+{
+}
+
+void IPickupInterface::AddSouls(ASoul* Soul)
+{
+}
+
+void IPickupInterface::AddGold(ATreasure* Treasure)
+{
+}
